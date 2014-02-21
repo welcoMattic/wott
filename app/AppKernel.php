0972c50499e58eabf8586affe33df55a2b5fc82a
Mathieu Santostefano <mathieu.santostefano@gmail.com>
2014-02-21 15:43:27 +0100

    [Bundle] installation FosFacebookBundle (fosuserbundle cassé)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new wott\FrontBundle\wottFrontBundle(),
             new wott\CoreBundle\wottCoreBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\FacebookBundle\FOSFacebookBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
