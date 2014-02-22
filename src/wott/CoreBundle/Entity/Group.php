28ed2431384b31130d91e97fd91972767af7b011
kcypher13 <cypher@ftad.fr>
2014-02-22 17:01:03 -0500

    hotfix sonata

---
+++ src/wott/CoreBundle/Entity/Group.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace wott\CoreBundle\Entity;
+
+use Sonata\UserBundle\Entity\BaseGroup as BaseGroup;
+
+class Group extends BaseGroup
+{
+    /**
+     * @var integer $id
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
