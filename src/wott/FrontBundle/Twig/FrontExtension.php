970802cef1cfc5ce7879331ceb1d3fc9129ffaec
kcypher13 <cypher@ftad.fr>
2014-02-22 19:29:11 -0500

    Fix FBconnect + sonata

---
+++ src/wott/FrontBundle/Twig/FrontExtension.php
@@ -0,0 +1,24 @@
+<?php
+namespace wott\FrontBundle\Twig;
+
+class FrontExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'ms_in_hours' => new \Twig_Filter_Method($this, 'ms_in_hoursFilter'),
+        );
+    }
+
+    public function ms_in_hoursFilter($ms) {
+        if ($ms > 3600)
+            $h = floor($ms / 3600);
+        $ms = $ms % 3600;
+        return trim($h.'h').gmdate('i', $ms);
+    }
+
+    public function getName()
+    {
+        return 'front_extension';
+    }
+}
