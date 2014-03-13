4fbb3a0a412b20fd1aa945fdf83c07a222c6ccf7
mbouras <marine.bouras@gmail.com>
2014-03-13 21:49:36 +0100

    Ajout des pages 'Films likes' et 'Films vus' dans l'espace Profil

--- src/wott/FrontBundle/Menu/Builder.php
+++ src/wott/FrontBundle/Menu/Builder.php
@@ -46,6 +46,8 @@ class Builder extends ContainerAware
         $menu = $factory->createItem('root');
 
         $menu->addChild('Ma Watchlist', array('route' => 'fos_user_profile_show'));
+        $menu->addChild('Films vus', array('route' => 'filmSeen'));
+        $menu->addChild('Mes Likes', array('route' => 'filmLike'));
         $menu->addChild('Mes informations', array('route' => 'sonata_user_profile_edit_authentication'));
 
         return $menu;
